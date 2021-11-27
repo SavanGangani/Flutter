b8cf456458f0db5a624fe2d60db704c161d34e5c
SavanGangani <savangangani8518@gmail.com>
2021-11-27 13:09:01 +0530

    added Xylophone project

---
+++ xylophone/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audioplayers_api.dart';
+
+void main() => runApp(const Xylophone());
+
+class Xylophone extends StatelessWidget {
+  const Xylophone({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child:  FlatButton(
+              color: Colors.blue,
+              onPressed: (){
+                //final play = AudioCache();
+                final as=AudioPlayer();
+                as.play("note7.wav");
+              },child: Text("Play"),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
